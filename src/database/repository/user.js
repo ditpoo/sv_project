c818b82a84f5143279bce32b258c138fc2af6182
ditpoo <ditpoo@gmail.com>
2023-01-18 12:29:56 +0530

    Add find method to user repository

--- src/database/repository/user.js
+++ src/database/repository/user.js
@@ -8,6 +8,20 @@ class UsersRepo {
     static async findById(id) {
         return UsersModel.findOne({ _id: id }).lean().exec();
     }
+
+    static async find(first_name, last_name, phone_number) {
+        if (first_name && !last_name && !phone_number) {
+            return UsersModel.findOne({ first_name: first_name.toString() }).lean().exec();
+        }
+
+        if (last_name && !first_name && !phone_number) {
+            return UsersModel.findOne({ last_name: last_name.toString() }).lean().exec();   
+        }
+
+        if (phone_number && !first_name && !last_name) {
+            return UsersModel.findOne({ phone_number: phone_number.toString() }).lean().exec();   
+        }
+    }
 }
 
-module.exports = UserRepo
+module.exports = UserRepo;
