75793f38b7535b70923aea9c1886396984a8bbe0
ditpoo <ditpoo@gmail.com>
2023-01-18 12:29:20 +0530

    Add create controller

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,5 +1,38 @@
-const bcrypt = ("bcrypt");
+const bcrypt = require("bcrypt");
+const UserRepo = require("../database/repository/user")
 
 async function create(req, res) {
+    const { 
+        first_name, 
+        middle_name,
+        last_name, 
+        phone_number, 
+        email,
+        password } = req.body;
+
+    if (
+        !first_name || 
+        !middle_name ||
+        !last_name ||
+        !phone_number ||
+        !email ||
+        !password) {
+            res.status(404).send({ "msg": "missing fields" });
+    } else {
+        // TODO: Hash the password using bycrpt
+        const user = UserRepo.create({
+            first_name, 
+            middle_name,
+            last_name, 
+            phone_number, 
+            email,
+            password
+        })
+
+        res.status(200).json(user);
+    }
 }
 
+module.exports = {
+    create,
+}
